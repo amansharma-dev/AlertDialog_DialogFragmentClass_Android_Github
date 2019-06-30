6bc83f172ba88524770420fb95bfe2f44e5e938e
Aman Sharma <amansharma.11@hotmail.com>
2019-06-30 19:54:53 +0530

    commit

--- app/src/main/java/com/example/alertdialog_dialogfragmentclass_android_github/AlertDialogFragment.java
+++ app/src/main/java/com/example/alertdialog_dialogfragmentclass_android_github/AlertDialogFragment.java
@@ -1,8 +1,37 @@
 package com.example.alertdialog_dialogfragmentclass_android_github;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.widget.Toast;
 
 public class AlertDialogFragment extends DialogFragment {
-    
 
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
+        AlertDialog alertDialog = new AlertDialog.Builder(getActivity()).create();
+        alertDialog.setTitle("Alert Dialog");
+        alertDialog.setMessage("Do you want to exit the dialog?");
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "No",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getContext(),"No",Toast.LENGTH_SHORT).show();
+                    }
+                });
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Yes",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getContext(),"Yes",Toast.LENGTH_SHORT).show();
+                    }
+                });
+        return alertDialog;
+    }
 }
--- app/src/main/java/com/example/alertdialog_dialogfragmentclass_android_github/MainActivity.java
+++ app/src/main/java/com/example/alertdialog_dialogfragmentclass_android_github/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.alertdialog_dialogfragmentclass_android_github;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton floatingActionButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        floatingActionButton = findViewById(R.id.floating_action_button);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+        });
+    }
+
+    private void showDialog(){
+        AlertDialogFragment alertDialogFragment = new AlertDialogFragment();
+        alertDialogFragment.setCancelable(false);
+        alertDialogFragment.show(getSupportFragmentManager(),"Alert_Dialog_Fragment");
     }
 }
